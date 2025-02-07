c8d9e4c6ae8a24c99756f182fdee651e4b08a39b
Raam <123749532+RaamHorakeri@users.noreply.github.com>
2025-02-07 12:04:39 +0530

    Update eslint.config.mjs

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -3,11 +3,24 @@ import pluginJs from "@eslint/js";
 import tseslint from "typescript-eslint";
 import pluginReact from "eslint-plugin-react";
 
-
 /** @type {import('eslint').Linter.Config[]} */
 export default [
-  {files: ["**/*.{js,mjs,cjs,ts,jsx,tsx}"]},
-  {languageOptions: { globals: globals.browser }},
+  {
+    files: ["**/*.{js,mjs,cjs,ts,jsx,tsx}"],
+    ignores: ["node_modules", "kubernetes-yaml"], // Ignore unnecessary files
+    languageOptions: {
+      ecmaVersion: "latest",
+      sourceType: "module",
+      globals: {
+        ...globals.node, // Include Node.js globals
+        ...globals.browser
+      }
+    },
+    rules: {
+      "no-undef": "off", // Prevents errors for Node.js globals like __dirname
+      "@typescript-eslint/no-require-imports": "off" // Allows `require()`
+    }
+  },
   pluginJs.configs.recommended,
   ...tseslint.configs.recommended,
   pluginReact.configs.flat.recommended,
